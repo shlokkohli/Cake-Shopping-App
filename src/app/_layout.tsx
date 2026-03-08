b0f5835f43676c3544f790bc10170e5e2499097b
Shlok Kohli <shlokkohli11@gmail.com>
2026-03-09 03:11:29 +0530

    Create based setup for auth store

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,31 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar"
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useAuthStore } from "../auth/authStore";
+import { useEffect } from "react";
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const isLoading = useAuthStore((state) => state.isLoading);
+  const restoreSession = useAuthStore((state) => state.restoreSession); 
+
+  useEffect(() => {
+    restoreSession();
+  }, [])
+
+  useEffect(() => {
+    if(!isLoading){
+      SplashScreen.hideAsync();
+    }
+  }, [isLoading])
+
+  if(isLoading){
+    return null;
+  }
 
   return (
     <SafeAreaProvider>
@@ -10,11 +33,11 @@ export default function RootLayout() {
       <Stack>
 
         {/* if guard is true, then this screen can be accesses */}
-        <Stack.Protected guard={false}>
+        <Stack.Protected guard={!isAuthenticated}>
           <Stack.Screen name="(public)" options={{ headerShown: false }} />
         </Stack.Protected>
 
-        <Stack.Protected guard={true}>
+        <Stack.Protected guard={isAuthenticated}>
           <Stack.Screen name="(protected)" options={{ headerShown: false }} />
         </Stack.Protected>
 
---
+++ src/auth/authStore.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand'
+
+interface User {
+    fullName: string,
+    phone: string
+}
+
+interface AuthStore {
+    isAuthenticated: boolean,
+    isLoading: boolean,
+
+    login: () => void,
+    logout: () => void,
+    restoreSession: () => Promise<void>
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+    isAuthenticated: false,
+    isLoading: true,
+
+    login: () => set({ isAuthenticated: true }),
+    logout: () => set({ isAuthenticated: false}),
+
+    restoreSession: async() => {
+        // first check user token
+        const token = null;
+        if(token){
+            set({ isAuthenticated: true });
+        } else {
+            set({ isAuthenticated: false });
+        }
+
+        set({ isLoading: false })
+    }
+}))
