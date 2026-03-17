35426b951bcee97f87084857e81fbf589c21fc24
Shlok Kohli <shlokkohli11@gmail.com>
2026-03-18 01:37:00 +0530

    Finished cake card component

--- src/components/CakeCard.tsx
+++ src/components/CakeCard.tsx
@@ -1,4 +1,4 @@
-import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, ImageSourcePropType, LayoutAnimation, Platform, Pressable, StyleSheet, Text, UIManager, View } from "react-native";
 import { Colors } from "../constants/Colors";
 import { Typography } from "../constants/Typography";
 import { useState } from "react";
@@ -9,12 +9,27 @@ interface ICakeCard {
     description: string
 }
 
+if(Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental){
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
 export default function CakeCard({ title, imagePath, description }: ICakeCard) {
+    
+    const [quantity, setQuantity] = useState<number>(0);
 
-    const [quantity, setQuantity] = useState<number>(0)
-
-    const increaseQuatity = () => setQuantity(prev => prev+ 1);
-    const decreaseQuantity = () => setQuantity(prev => (prev > 0 ? prev - 1 : 0))
+    const animate = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+    }
+    
+    const increaseQuatity = () => {
+        animate();
+        setQuantity(prev => prev+ 1);
+    }
+    
+    const decreaseQuantity = () => {
+        animate();
+        setQuantity(prev => (prev > 0 ? prev - 1 : 0));
+    }
 
     return (
         <View style={styles.container}>
@@ -37,26 +52,46 @@ export default function CakeCard({ title, imagePath, description }: ICakeCard) {
                     {description}
                 </Text>
 
-                <View>
+                <View style={styles.buttonWrapper}>
                     {quantity === 0 ? (
-                        <Pressable>
-                            Add to cart
+                        <Pressable
+                            style={styles.addToCartButton}
+                            onPress={increaseQuatity}
+                        >
+                            <Text style={styles.addToCartText}>
+                                Add to cart
+                            </Text>
                         </Pressable>
                     ): (
-                        <Text>Hello</Text>
-                        // <View>
-                        //     <Pressable>
+                        <View style={styles.quantityControls}>
+                            <Pressable
+                                onPress={decreaseQuantity}
+                                style={styles.stepButton}
+                            >
+                                <Text
+                                    style={styles.stepButtonText}
+                                >
+                                    -
+                                </Text>
+                            </Pressable>
 
-                        //     </Pressable>
+                            <Text
+                                style={styles.quantityText}
+                            >
+                                {quantity}
+                            </Text>
 
-                        //     <Text>
-
-                        //     </Text>
-
-                        //     <Pressable>
-
-                        //     </Pressable>
-                        // </View>
+                            <Pressable
+                                onPress={increaseQuatity}
+                                style={styles.stepButton}
+                            >
+                                <Text
+                                    style={styles.stepButtonText}
+                                >
+                                    +
+                                </Text>
+                            </Pressable>
+                        </View>
                     )}
                 </View>
 
@@ -68,10 +103,12 @@ export default function CakeCard({ title, imagePath, description }: ICakeCard) {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: Colors.light.primaryDisabled,
+        backgroundColor: "#FDFBF7",
         padding: 10,
         borderRadius: 15,
-        width: 190
+        width: 190,
+        borderWidth: 1,
+        borderColor: '#E5E7EB',
     },
     imageStyle: {
         height: 180,
@@ -101,5 +138,42 @@ const styles = StyleSheet.create({
         fontWeight: Typography.weight.regular,
         color: Colors.light.textSecondary,
         flex: 1,
-    }
+    },
+    buttonWrapper: {
+        marginTop: 10,
+    },
+    addToCartButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: Colors.light.primary,
+        borderRadius: 8,
+        height: 36
+    },
+    addToCartText: {
+        fontSize: Typography.size.s,
+        fontWeight: Typography.weight.medium
+    },
+    quantityControls: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: Colors.light.primary,
+        height: 36,
+    },
+    stepButton: {
+        paddingHorizontal: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    stepButtonText: {
+        fontSize: Typography.size.l,
+        fontWeight: Typography.weight.regular
+    },
+    quantityText: {
+        fontSize: Typography.size.m,
+        fontWeight: Typography.weight.medium
+    },
 })
