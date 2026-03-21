89207718f2a664659b54446c0a31aeafaf423f79
Shlok Kohli <shlokkohli11@gmail.com>
2026-03-21 23:21:00 +0530

    Changed profile page

--- src/modules/protected/Profile/Components/Profile.tsx
+++ src/modules/protected/Profile/Components/Profile.tsx
@@ -3,9 +3,7 @@ import { Text, View } from "react-native";
 export default function Profile() {
     return (
         <View>
-            <Text>
-                This is Profile Screen
-            </Text>
+            <Text> This is Profile Screen </Text>
         </View>
     )
 }
